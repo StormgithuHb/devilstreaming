f4134ae398afd1f8bed3c932125236c5fa28dd54
StormgithuHb <109058708+StormgithuHb@users.noreply.github.com>
2022-07-16 19:51:47 +0300

    Create Devil.js

--- Devil.js
+++ Devil.js
@@ -1,1 +1,32 @@
 
+const Discord = require('discord.js');
+const tpoints = {};
+const prefix = "2"
+const vpoints = {};
+const jimp = require("jimp");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+  console.log(
+    `Online In Servers : ${client.guilds.size} | Users : ${client.users.size}`
+  );
+  let statuses = [
+   
+    `devil`,
+    `devil`,
+    `devil`,
+    `devil `,
+    `Devil `,
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    client.user.setActivity(STREAMING, {      
+    type:"Playing", 
+    url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+
+client.login("");
